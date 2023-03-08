93f6e10900e3ca5c4654347ce9362199826222af
Riley White <rileywhite89@gmail.com>
2023-03-08 14:57:41 -0500

    File for testing blazepose angles (tests suck)

---
+++ tests/test_blazepose.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import joints as j
+from skillest.task_decomp.blaze_pose_seg import get_all_2d_angles, segment
+from skillest.task_decomp.segmentation import Segmentation
+
+
+jj = pd.read_csv("tests/jumping_jack_blaze.csv", index_col="timestamp").values[25:]
+data, angle_dict, idx_dict = get_all_2d_angles(jj)
+
+
+if __name__ == "__main__":
+    # seg = Segmentation(k=2)
+    # points, _ = seg.segment(data, return_properties=True)
+
+    # seg.plot_segmentation(np.tile(data, [1, 1]), angle_dict.keys(), True)
+    # plt.show()
+    frame = jj[56]
+    jc = j.JointCollection(j.frame_to_dict(frame), j.BLAZEPOSE_CONNECTIONS)
+    s = j.BLAZE_HIP()
+    s.set_joint_collection(jc)
+    # print(jc["left_shoulder"])
+    # print(jc["left_hip"])
+    # print(jc["left_knee"])
+    print( (np.array(list(s.eval().values())) * 180) / np.pi)
+    # print((180 * np.arccos(jc["left_hip", "left_shoulder", "left_elbow"].as_quat()[-1]) * 2) / np.pi)
+    # print((180 * np.arccos(jc["right_hip", "right_shoulder", "right_elbow"].as_quat()[-1]) * 2) / np.pi)
